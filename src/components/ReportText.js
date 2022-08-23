f5ddb123e9ec277851a9cfdd4d0f08a0a6e7959a
Rilr20 <nrrl99@gmail.com>
2022-08-23 13:17:38 +0200

    report and reporttext

---
+++ src/components/ReportText.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ReportText(props) {
+
+    const text = [
+        {title: "Report 1",text: "detta är report 1" },
+        {title: "Report 2",text: "detta är report 2"},
+        {title: "Report 3",text: "detta är report 3"},
+        {title: "Report 4",text: "detta är report 4"},
+        {title: "Report 5",text: "detta är report 5"},
+        {title: "Report 6",text: "detta är report 6"},
+        {title: "Report 10",text: "detta är report 10"},
+    ]
+    return (
+        <div>
+            <h2>{text[props.num].title}</h2>
+            <p>{text[props.num].text}</p>
+        </div>
+    )
+}
--- src/pages/Report.js
+++ src/pages/Report.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import ReportText from '../components/ReportText'
 
 export default function Report(props) {
-  return (
-    <div>Report {props.num}</div>
-  )
+    const [num, setNum] = useState(0);
+    // useEffect(() => {
+    // }, []);
+
+    function sayHello(number) {
+        setNum(number)
+    }
+    return (
+        <div>
+            <h1>Report</h1>
+            <div className="report-buttons">
+                <button className="report-button" onClick={() => sayHello(0)}>Report 1</button>
+                <button className="report-button" onClick={() => sayHello(1)}>Report 2</button>
+                <button className="report-button" onClick={() => sayHello(2)}>Report 3</button>
+                <button className="report-button" onClick={() => sayHello(3)}>Report 4</button>
+                <button className="report-button" onClick={() => sayHello(4)}>Report 5</button>
+                <button className="report-button" onClick={() => sayHello(5)}>Report 6</button>
+                <button className="report-button" onClick={() => sayHello(6)}>Report 10</button>
+            </div>
+            <ReportText num={num} />
+        </div>
+    )
+}
+
+function buttonClick(number) {
+    console.log(number);
 }
