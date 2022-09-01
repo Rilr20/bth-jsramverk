25c6612ff892bb4c9fa0852879ca605b244d4d42
Rilr20 <nrrl99@gmail.com>
2022-09-01 15:33:30 +0200

    Class component became Functional component

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { TrixEditor } from "react-trix";
 import "trix/dist/trix";
 
-export default class Editor extends Component {
-    handleEditorReady(editor) {
-        editor.insertString("editor is ready");
-    }
-    handleChange(html, text) {
-    }
-    buttonClick() {
+export default function Editor() {
+    const [documentId, setDocumentId] = useState(null);
+    function saveFile() {
         let editor = document.getElementsByClassName('textEditor')
         console.log(editor[0].innerText)
+        if (documentId === null) {
+            //spara ny fil
+            console.log("wow new file");
+        } else {
+            //spara över gammal
+            console.log("old file time to rewrite");
+        }
     }
-    render() {
-        return (
-            <div className='container'>
-                <h1>Editor</h1>
-                <div className='toolbar'>
-                    <h2>Toolbar </h2>
-                    <ul>
-                        <button onClick={this.buttonClick}>Save</button>
-                        {/* <li></li>
+    return (
+        <div className='container'>
+            <h1>Editor</h1>
+            <div className='toolbar'>
+                <h2>Toolbar </h2>
+                <ul>
+                    <div><input id="filename" placeholder='filename'></input> <button onClick={saveFile}>Save</button></div>
+                    {/* <li></li>
                         <li>Delete</li>
                         <li>Etc</li> */}
-                    </ul>
-                </div>
-                <TrixEditor className='textEditor' onChange={this.handleChange} onEditorReady={this.handleEditorReady} />
+                </ul>
             </div>
-        );
-    }
+            <div className='toolbar'>
+                <h2>Documents</h2>
+                <ul>
+                    <button onClick={()=> {setDocumentId("variable that should be here from the api")}}>Document Title</button>
+                </ul>
+            </div>
+            <p>Document id: {documentId === null ? "New File" : documentId}</p>
+            <TrixEditor className='textEditor' />
+        </div>
+    );
 }
