25896e5b8f34cee62f6258d56d3ddc7fab6ba05f
Rilr20 <nrrl99@gmail.com>
2022-09-14 21:40:29 +0200

    editor test

--- src/pages/__test__/Editor.test.js
+++ src/pages/__test__/Editor.test.js
@@ -1,3 +1,21 @@
-test('test', () => {
-    expect(true).toBe(true)
+import { render, screen, cleanup } from '@testing-library/react'
+import Editor from '../Editor'
+
+afterEach(() => {
+    cleanup();
+})
+
+// jest.mock('../../components/editorData', ()=> "fetchData")
+// const documents = [
+//     { title: "title", text: "text" },
+//     {title:"title", text:"text"},
+//     {title:"title", text:"text"},
+//     { title: "title", text: "text" }
+// ]
+// const fetchData = () => false;
+
+test('Document button', async () => {
+    render(<Editor />)
+    const documentList = screen.getByTestId('document-list');
+    expect(documentList).toHaveTextContent('New Document')
 })
