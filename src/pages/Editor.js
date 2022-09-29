84a365ac7a4a3a7619d42f61c7079e4e1b3d9235
Rilr20 <nrrl99@gmail.com>
2022-09-29 21:16:30 +0200

    pages edits

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -13,22 +13,24 @@ function changeSendToSocket(value) {
     sendToSocket = value;
 }
 
-export default function Editor() {
+export default function Editor({ token, setToken, email, setEmail }) {
     const [documents, setDocuments] = useState([]);
     const editor = document.getElementsByClassName('textEditor')
     const fileNameInput = document.getElementById('filename')
-    const [formInput, updateFormInput] = useState({_id:null, title: '', text: '' })
+    const [formInput, updateFormInput] = useState({ _id: null, title: '', text: '', email: email, code: false })
     const [socket, setSocket] = useState(null);
+    const [useSocket, setUseSocket] = useState(false);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         (async () => {
             console.log("run");
-            await fetchData(setDocuments)
+            await fetchData(setDocuments, token)
         })();
     }, []);
 
     useEffect(() => {
-        setSocket(io("https://jsramverk-editor-rilr20a.azurewebsites.net/"))
+        setSocket(io(""))
 
         return () => {
             if (socket) {
@@ -39,7 +41,7 @@ export default function Editor() {
     }, []);
     useEffect(() => {
         console.log("socket");
-        if(socket) {
+        if (socket) {
             socket.on("doc", function (data) {
                 // console.log(data);
                 changeSendToSocket(false)
@@ -53,8 +55,8 @@ export default function Editor() {
     function emitToSocket() {
         console.log("tja");
 
-        if (socket && sendToSocket) {
-            // console.log("socket time");
+        if (socket && sendToSocket && useSocket) {
+            console.log("socket time");
             socket.emit("doc", { _id: formInput._id, text: formInput.text })
         }
         changeSendToSocket(true)
@@ -68,10 +70,12 @@ export default function Editor() {
         console.log(editor[0].innerText)
         let text = formInput.text
         let title = formInput.title
+        let email = formInput.email
+        let code = formInput.code
         if (formInput._id === null) {
             //spara ny fil
 
-            let data = { title, text }
+            let data = { title, text, email, code }
             let requestOptions = {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -80,9 +84,13 @@ export default function Editor() {
 
             const response = await fetch('https://jsramverk-editor-rilr20a.azurewebsites.net/docs', requestOptions)
             const body = await response.text()
-
-            if (response.status === 201) window.location.reload()
-            else if (response.status !== 201) throw Error(body.message)
+            console.log(response);
+            if (response.status === 201) {
+                setOpen(true)
+                setTimeout(() => setOpen(false), 5000);
+            }
+            // if (response.status === 201) window.location.reload()
+            // else if (response.status !== 201) throw Error(body.message)
 
             console.log("wow new file");
         } else {
@@ -99,9 +107,13 @@ export default function Editor() {
 
             const response = await fetch(`https://jsramverk-editor-rilr20a.azurewebsites.net/docs/${formInput._id}`, requestOptions)
             const body = await response.text()
-
-            if (response.status === 204) window.location.reload()
-            else if (response.status !== 204) throw Error(body.message)
+            console.log(response);
+            if (response.status === 204) {
+                setOpen(true)
+                setTimeout(() => setOpen(false), 5000);
+            }
+            // if (response.status === 204) window.location.reload()
+            // else if (response.status !== 204) throw Error(body.message)
         }
     }
     /**
@@ -109,16 +121,18 @@ export default function Editor() {
      * @param {string} documentId 
      */
     function changeDocument(documentId) {
-        updateFormInput({...formInput, id: documentId})
+        updateFormInput({ ...formInput, id: documentId })
         let document = findId(documents, documentId)
         try {
-            updateFormInput({...document})
+            setUseSocket(true)
+            updateFormInput({ ...document })
             socket.emit("create", documentId);
             console.log(fileNameInput);
             fileNameInput.value = document.title
             editor[0].innerText = document.text
 
         } catch (error) {
+            setUseSocket(false)
             fileNameInput.value = null
             editor[0].innerText = null
         }
@@ -129,42 +143,74 @@ export default function Editor() {
      * @param {string} text 
      */
     function handleChange(html, text) {
-            const copy = Object.assign({}, formInput);
+        const copy = Object.assign({}, formInput);
 
-            copy.text = text;
-            updateFormInput(copy);
+        copy.text = text;
+        updateFormInput(copy);
     }
 
+    function renderDocs() {
+        let render = []
+        console.log(documents);
+        try {
+
+            documents.forEach(element => {
+                render.push(<option className='existing-document' key={document._id} onClick={() => { changeDocument(document._id) }}>{document.title}</option>)
+            });
+        } catch (error) {
+
+        }
+        return render;
+    }
     return (
         <div className='container'>
-            <div className='toolbar'>
-                <div className='tools'>
-                    <div className='document-list'>
-                        <input className='filename' id="filename" placeholder='title' name="title" onChange={(e) => { updateFormInput({ ...formInput, title: e.target.value })}}></input>
-                        <button className='filename-btn' onClick={saveFile}>Save</button>
-                    </div>
-                    <div data-testid="document-list" className='document-list'>
-                        
-                        <select className='dropdown-documents'>
-                            <option autoFocus>Switch Between Documents</option>
-                            {
-                                documents.map((document) => {
-                                    return <option className='existing-document' key={document._id} onClick={() => { changeDocument(document._id) }}>{document.title}</option>
-                                })
-                            }
-                        </select>
-                        <button className='new-document' onClick={() => { changeDocument(null) }}>New Document</button>
+            {token === "" ? <h1>Login to access this page</h1> : 
+            <>
+                <div className='toolbar'>
+                    <div className='tools'>
+                        <div className='document-list'>
+                            <input className='filename' id="filename" placeholder='title' name="title" onChange={(e) => { updateFormInput({ ...formInput, title: e.target.value }) }}></input>
+                            <button className='filename-btn' onClick={saveFile}>Save</button>
+                        </div>
+                        <div data-testid="document-list" className='document-list'>
+
+                            <select className='dropdown-documents'>
+                                <option autoFocus>Switch Between Documents</option>
+                                {
+                                    // renderDocs()
+                                    // () => {
+                                    //     console.log("giaasdhglöasdhglköaasdghlökashdg");
+                                    //     try {
+                                    //         documents.map((document) => {
+                                    //             return <option className='existing-document' key={document._id} onClick={() => { changeDocument(document._id) }}>{document.title}</option>
+                                    //         })
+                                    //     } catch (error) {
+                                    //         console.log(error);
+                                    //     }
+                                    // }
+                                    documents.map((document) => {
+                                        if (document.email === email) {
+                                            return <option className='existing-document' key={document._id} onClick={() => { changeDocument(document._id) }}>{document.title}</option>
+                                        }
+                                    })
+
+                                }
+                            </select>
+                            <button className='new-document' onClick={() => { changeDocument(null) }}>New Document</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <Permission />
-            <div onKeyUp={emitToSocket}>
-                <TrixEditor onChange={handleChange} name="text" className='textEditor' placeholder='Dags att börja skriva' />
+                <Permission />
+                <div onKeyUp={emitToSocket}>
+                    <TrixEditor onChange={handleChange} name="text" className='textEditor' placeholder='Dags att börja skriva' />
 
+                </div>
+                <h1>{open ? "document saved" : ""}</h1>
+                <p>Document id: {formInput._id === null ? "New File" : formInput._id}</p>
+                <p>text:{formInput.text}</p>
+                <p>title:{formInput.title}</p>
+                </>
+            }
             </div>
-            <p>Document id: {formInput._id === null ? "New File" : formInput._id}</p>
-            <p>text:{formInput.text}</p>
-            <p>title:{formInput.title}</p>
-        </div>
     );
 }
